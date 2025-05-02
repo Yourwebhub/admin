b6c99d21b609d6c40967924bf5239b16ef139459
Your web hub <mitraroni46@gmail.com>
2025-05-02 19:09:44 +0530

    Update Team.jsx

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,13 +21,13 @@ const Team = () => {
       image: "/team/roni-bhi.jpg",
       category: "DT",
     },
-    {
-      id: "2",
-      designation: "Founder",
-      employe: "Surendar singh",
-      image: "/team/surinder.jpg",
-      category: "DT",
-    },
+    // {
+    //   id: "2",
+    //   designation: "Founder",
+    //   employe: "Surendar singh",
+    //   image: "/team/surinder.jpg",
+    //   category: "DT",
+    // },
     // {
     //   id: "3",
     //   designation: "HR",
